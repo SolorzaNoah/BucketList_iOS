81bc4e66005bfaddaf320eda64cabe6a8ab7649b
student <student@823282TWU.local>
2020-03-01 19:59:08 -0600

    @nsolorzano1_push_03012020

---
+++ Bucketlist/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  Test
+//
+//  Created by Jonathan Ninan on 2/27/20.
+//  Copyright © 2020 Jonathan Ninan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var _: String = ""
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func FloatingActionButton(_ sender: Any) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bucketlist/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Jonathan Ninan on 2/25/20.
+//  Copyright © 2020 Jonathan Ninan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var MyTableView: UITableView!
+    
+    var data = ["Basketball", "Soccer", "Football", "Volleyball", "Jogging", "Basketball", "Rockclimbing", "Videogame", "Scienceproject"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MyTableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func FloatingActionButton(_ sender: Any) {
+        newItem()
+    }
+    
+    
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    /*func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            self.data.remove(at: indexPath.row)
+            self.MyTableView.reloadData()
+        }
+    }*/
+    /*func tableView(_ tableView: UITableView, leadingSwipeActionConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let edit = UIContextualAction(style: .normal, title: "Edit")
+        { (contextualAction, view, actionPerformed: (Bool) -> ()) in self.performSegue(withIdentifier: "toAddztripSegue", sender: nil)
+        }
+        return UISwipeActionsConfiguration(actions: [edit])
+    }*/
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let modifyAction = UIContextualAction(style: .normal, title:  "Update", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+            print("Update action ...")
+            success(true)
+        })
+        modifyAction.image = UIImage(named: "hammer")
+        modifyAction.backgroundColor = .blue
+    
+        return UISwipeActionsConfiguration(actions: [modifyAction])
+    }
+
+    func newItem(){
+        data.append("Hockey")
+        
+        MyTableView.beginUpdates()
+        MyTableView.insertRows(at: [IndexPath(row: data.count-1, section: 0)], with: .automatic)
+        MyTableView.endUpdates()
+        
+        
+    }
+    
+    
+}
