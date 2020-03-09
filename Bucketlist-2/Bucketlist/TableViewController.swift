a057a44ac4ff9183efb07dfb2fdc598f6df1c21d
student <student@823282TWU.local>
2020-03-08 22:10:31 -0500

    readd delete function

--- Bucketlist-2/Bucketlist/TableViewController.swift
+++ Bucketlist-2/Bucketlist/TableViewController.swift
@@ -99,9 +99,16 @@ class TableViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     }
 
     // Override to support editing the table view
-
-
-    /*
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            data.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+   /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
